653bbabaf48e095bd02e20a8f397dcc088abda9e
Vansir <vansir7@gmail.com>
2018-01-20 17:26:04 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using System.Xml.Linq;
+
+namespace LogsChanger9000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "C:/Users/Vansir/Desktop/LogsChanger9000/test.xml";
+            string newFilePath = "C:/Users/Vansir/Desktop/LogsChanger9000/results.xml";
+            string stageName = "Start";
+
+            XDocument xmlFile = XDocument.Load(filePath);
+            
+            var allStages = from element in xmlFile.Elements("process").Elements("stage")
+            select element;
+
+            foreach (XElement element in allStages) 
+            {
+                if (element.Attribute("type").Value == stageName)
+                {   
+                    var childElements = from child in element.Elements("loginhibit")
+                    select child;
+                    foreach (XElement child in childElements)
+                    {
+                        child.Remove();
+                    }
+                    //linijka ponizej tylko do testow
+                    element.Attribute("type").Value = "a co to sie stalo?";
+                }
+            }
+            xmlFile.Save(newFilePath);
+        }
+    }
+}
